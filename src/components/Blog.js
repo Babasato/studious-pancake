7cf5fcbaec8af68818a59353489a53990d4eef17
Babasato <114899243+Babasato@users.noreply.github.com>
2024-11-12 21:12:28 +0300

    Create Blog.js

---
+++ src/components/Blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Blog() {
+  return (
+    <section id="blog" style={{ padding: "2rem", backgroundColor: "#fff3e0" }}>
+      <h2>Our Blog</h2>
+      {/* Add blog posts here */}
+    </section>
+  )
+}
